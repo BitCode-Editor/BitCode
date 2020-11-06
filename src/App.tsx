51462f4e3bcf0bc7138ad8ebb455ecdcda6f3ca6
thijsrijkers <thijsrijkers@live.nl>
2020-11-06 23:00:40 +0100

    +Added some read and write functions

--- src/App.tsx
+++ src/App.tsx
@@ -42,4 +42,39 @@ function loadFile() {
   input.click();
 }
 
+function saveTextAsFile() 
+{
+  var textToWrite = document.getElementById('typeArea').innerHTML;
+  var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+  var fileNameToSaveAs = "ecc.plist";
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+}
+
+var button = document.getElementById('save');
+button.addEventListener('click', saveTextAsFile);
+
+
+function loadFileAndSet() {
+  var input = document.createElement('input');
+  input.type = 'file';
+  
+  var textarea = document.querySelector('typeArea') 
+
+  input.click();
+}
+
 export default App;
