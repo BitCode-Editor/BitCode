441d1915c7db2e6a8255deb81fb41a222bce1584
thijsrijkers <thijsrijkers@live.nl>
2020-11-07 13:25:19 +0100

    -Removed index file +- change main file

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import { app, BrowserWindow } from 'electron';
 
-const createWindow = (): void => {
-  let win = new BrowserWindow({
+
+function createWindow () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
-  });
+  })
 
-  win.loadFile('index.html');
+  win.loadFile('index.html')
+  win.webContents.openDevTools()
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
